d17e2121ff1a79e0b584d0a6b3e059bffd557119
cohenerickson <cohenerickson@gmail.com>
2023-11-01 13:09:52 -0500

    XYZ transformations

--- src/util.ts
+++ src/util.ts
@@ -2,20 +2,73 @@
 import { randomScrambleForEvent } from "cubing/scramble";
 
 export function processSequence(sequence: string): string {
-  const alg = simplify(
-    sequence
-      .toString()
-      .replace(/u2/gi, "R L F2 B2 R' L' D2 R L F2 B2 R' L'")
-      .replace(/u'/gi, "R L F2 B2 R' L' D' R L F2 B2 R' L'")
-      .replace(/u/gi, "R L F2 B2 R' L' D R L F2 B2 R' L'")
-      .split(/\s/g)
-  );
+  const alg = simplify(transform(sequence.toLowerCase().split(/\s/g)));
 
-  return alg.join(" ");
+  return alg.join(" ").toUpperCase();
 }
 
 export async function generateScramble(): Promise<string> {
   return processSequence(await randomScrambleForEvent("333"));
+}
+
+export function transform(alg: string[], transformation?: string[]) {
+  const transformedMoves: string[] = [];
+
+  if (transformation) {
+    for (let i = 0; i < alg.length; i++) {
+      let move = (alg[i].match(/^[a-z]+/i) ?? "")[0];
+      const double = /^[a-z]+2$/i.test(alg[i]);
+      const prime = /^[a-z]+'$/i.test(alg[i]);
+
+      if (!move) continue;
+
+      const index = transformation.indexOf(move);
+
+      if (index >= 0) {
+        move = transformation[(index + 1) % transformation.length];
+        alg[i] = move + (double ? "2" : prime ? "'" : "");
+      }
+    }
+  }
+
+  for (let i = 0; i < alg.length; i++) {
+    const move = (alg[i].match(/^[a-z]+/i) ?? "")[0];
+    const double = /^[a-z]+2$/i.test(alg[i]);
+    const prime = /^[a-z]+'$/i.test(alg[i]);
+
+    if (!move) continue;
+
+    if (move === "u") {
+      transformedMoves.push(
+        ...`R L F2 B2 R' L' D${
+          double ? "2" : prime ? "'" : ""
+        } R L F2 B2 R' L'`.split(/\s/g)
+      );
+      continue;
+    } else if (move === "x") {
+      transformation = ["u", "f", "d", "b"];
+      if (prime) transformation = transformation.reverse();
+    } else if (move === "y") {
+      transformation = ["r", "f", "l", "b"];
+      if (prime) transformation = transformation.reverse();
+    } else if (move === "z") {
+      transformation = ["u", "r", "d", "l"];
+      if (prime) transformation = transformation.reverse();
+    } else {
+      transformedMoves.push(move + (double ? "2" : prime ? "'" : ""));
+      continue;
+    }
+
+    transformedMoves.push(
+      ...transform(
+        double ? [move, ...alg.slice(i + 1)] : alg.slice(i + 1),
+        transformation
+      )
+    );
+    break;
+  }
+
+  return transformedMoves;
 }
 
 export function simplify(alg: string[]): string[] {
